03819cdf557ff5e25a2a7c225d30ada42935e798
VitorSantox <vitor2004hugo.vh13@gmail.com>
2025-08-21 17:09:10 +0000

    commit

--- terraforms-lab/main.tf
+++ terraforms-lab/main.tf
@@ -2,13 +2,25 @@ resource "google_container_cluster" "primary" {
   name = var.cluster_name
   location = var.region
 
-  #remove_default_node_pool = true
-  initial_node_count = 2
- 
+  #remove o node pool padrão
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+
+resource "google_container_node_pool" "primary_nodes" {
+  name = "${var.cluster_name}-pool"
+  location = var.region
+  cluster = google_container_cluster.primary.name
+  node_count = car.node_count
+
+
   node_config {
-    machine_type = "e2-micro"
-    disk_size_gb = 50
-    disk_type = "pd-standard"
+    machine_type = var.machine_type
+    disk_size_gb = var.disk_size
+    disk_type = var.disk_type
+    ouath_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      ]
   }
 }
  
