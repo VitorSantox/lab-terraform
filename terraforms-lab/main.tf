6fc8d0c681ce6c92d73925ce72fa0d2dc10df8ff
Vitor Santos <88788394+VitorSantox@users.noreply.github.com>
2025-08-21 13:32:39 -0300

    Update main.tf

--- terraforms-lab/main.tf
+++ terraforms-lab/main.tf
@@ -2,7 +2,7 @@ resource "google_container_cluster" "primary" {
   name = var.cluster_name
   location = var.region
 
-  remove_default_node_pool = true
+  #remove_default_node_pool = true
   initial_node_count = 2
  
   node_config {
