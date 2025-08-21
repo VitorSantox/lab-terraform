be6f5ec260df23a6b7ecca9a64106cb7961d461b
VitorSantox <vitor2004hugo.vh13@gmail.com>
2025-08-21 17:29:35 +0000

    commit

--- terraforms-lab/main.tf
+++ terraforms-lab/main.tf
@@ -6,18 +6,20 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count = 1
 
-
+#criando manualmente a config do pool de no com-pool pra diferenciar do nome do cluster
 resource "google_container_node_pool" "primary_nodes" {
   name = "${var.cluster_name}-pool"
   location = var.region
-  cluster = google_container_cluster.primary.name
-  node_count = car.node_count
+  cluster = google_container_cluster.primary.name #liga este node_pool ao clsuter que criamos acima
+  node_count = var.node_count
 
-
+  #configuração dos no (maquina virtuais que compoes o cluster)
   node_config {
     machine_type = var.machine_type
     disk_size_gb = var.disk_size
     disk_type = var.disk_type
+    #Escopo de autenticacao que os nos terao para aecssar apis na gcp
+    #aqui usamos "cloud-plataform", que dá acesso amplo (bom para labs, mas em prd o ideal e restringir
     ouath_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
       ]
